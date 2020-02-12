95160a4b4bef02a3b76c8967ee33c97dc0ece33d
Work <a.kadochnikova@sheffield.ac.uk>
2020-02-12 12:32:36 +0000

    Update to local init
    Checks folders already in path to avoid clutter

--- local_init.m
+++ local_init.m
@@ -1,13 +1,26 @@
 %% Add mylibs to and subdirectories to path in local directory
 % Requires that mylibs is located in the parent folder.
-LibFolder = '../mylibs';
-addpath(LibFolder)
+LibFolder = 'mylibs';                                                       % name of library name
+pathCell = regexp(path, pathsep, 'split')';                                 % array of directories in Matlab path
+index_path = find(cell2mat(strfind(pathCell,LibFolder)));                   % find whether path contains mylibs
+LibFolder = ['../',LibFolder];
+if isempty(index_path)
+    addpath(LibFolder)
+    newPath = true;
+else
+    myPath = pathCell(index_path);                                          % see all subridectories in the path
+    newPath = false;
+end
+%% Go through the subdirectories
 f = dir(LibFolder);
-git = 4;
-N = length(f)-git;
-for iDir=1:N
-    if f(iDir+git).isdir
-       addpath([LibFolder,'/',f(iDir+git).name])
+git = 4;                                                                    % git offset
+N = length(f);
+for iDir=git:N
+    if f(iDir).isdir
+       if newPath || ~any(cell2mat(strfind(myPath,f(iDir).name)))           % if directory is not in path
+        addpath([LibFolder,'/',f(iDir).name]);
+       end
     end
 end
+%% Figure and interpreter setup
 my_init;
